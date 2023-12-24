a92a3a50da0b41e213b413f2220ff6d8241e5004
kshashwat007 <kshashwt007@gmail.com>
2023-12-24 19:08:32 +0530

    layout adjust

--- app/summary-feed/page.tsx
+++ app/summary-feed/page.tsx
@@ -4,22 +4,21 @@ import React, { useState } from 'react';
 
 const EmailSummary = ({ email }) => {
   return (
-    <div className="card bg-base-200 p-4 mt-3 w-full max-w-xs shadow rounded-lg overflow-hidden">
-      <div className="flex justify-between items-start mb-2">
+    <div className="card bg-base-200 p-6 mt-3 w-full max-w-md shadow rounded-lg overflow-hidden">
+      <div className="flex justify-between items-start mb-4">
         <div>
           <div className="text-sm font-medium text-gray-800">{email.sender || 'Sender'}</div>
           <div className="text-lg font-semibold text-gray-900">{email.subject || 'Subject'}</div>
         </div>
-        <div className="text-xs text-gray-400 self-end">{email.date || 'Date/Time'}</div>
+        <div className="text-xs text-gray-400">{email.date || 'Date/Time'}</div>
       </div>
-      <div className="divider"></div> 
-      <p className="text-gray-600 mt-1 line-clamp-2">{email.summary || 'No summary provided.'}</p>
-      <div className="divider"></div> 
-      {/* If there are action items, display up to two */}
+      <div className="divider"></div>
+      <p className="text-gray-600 line-clamp-2 mb-4">{email.summary || 'No summary provided.'}</p>
+      <div className="divider"></div>
       {email.action_items && email.action_items.length > 0 && (
-        <div className="mt-2">
-          <h3 className="text-sm font-semibold text-gray-700">Action Items (showing 2)</h3>
-          <ul className="list-disc pl-5 mt-1">
+        <div>
+          <h3 className="text-sm font-semibold text-gray-700 mb-1">Action Items (showing 2)</h3>
+          <ul className="list-disc pl-6 space-y-1">
             {email.action_items.slice(0, 2).map((item, index) => (
               <li key={index} className="text-xs text-gray-600">{item}</li>
             ))}
@@ -27,20 +26,20 @@ const EmailSummary = ({ email }) => {
         </div>
       )}
 
-      {/* If there are links, display up to two */}
       {email.links && email.links.length > 0 && (
-        <div className="mt-2">
-          <h3 className="text-sm font-semibold text-gray-700">Extracted Links (showing 2)</h3>
-          <ul className="list-disc pl-5 mt-1">
+        <div className="mt-4">
+          <h3 className="text-sm font-semibold text-gray-700 mb-1">Extracted Links (showing 2)</h3>
+          <ul className="list-disc pl-6 space-y-1">
             {email.links.slice(0, 2).map((item, index) => (
-              <li key={index} className="text-xs text-gray-600"><a href={item} target="_blank" rel="noopener noreferrer">{item}</a></li>
+              <li key={index} className="text-xs text-gray-600">
+                <a href={item} target="_blank" rel="noopener noreferrer">{item}</a>
+              </li>
             ))}
           </ul>
         </div>
       )}
 
-      {/* Expand button/icon */}
-      <div className="mt-2">
+      <div className="mt-4">
         <button className="text-orange-500 hover:text-orange-600 flex items-center transition-colors duration-200">
           Open Summary
           <svg xmlns="http://www.w3.org/2000/svg" className="ml-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
